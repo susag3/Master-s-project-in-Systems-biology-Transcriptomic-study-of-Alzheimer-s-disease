c79a147d104c097f7e97d1c2caac92e757f3c3fa
susag3 <60703095+susag3@users.noreply.github.com>
2020-12-12 22:02:27 +0100

    Update plotdegreedist.py

--- plotdegreedist.py
+++ plotdegreedist.py
@@ -1,24 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.ticker import ScalarFormatter
 
 X, Y = [], []
-for line in open('degreedist_data.txt', 'r'):
+#open file from Cytoscape where first/second column is degree/number of nodes
+for line in open('degreedist_data.txt', 'r'): 
     values = [float(s) for s in line.split()]
     X.append(values[0])
     Y.append(values[1])
 
 fig, ax = plt.subplots()
-ax.loglog(X,Y, 'o', color='black')
+ax.loglog(X,Y, 'o', color='black') #log-log scale
 ax.axis ([1,70, 1, 1000])
 plt.xlabel('Degree')
 plt.ylabel('Number of nodes')
 
-from matplotlib.ticker import ScalarFormatter
-for axis in [ax.xaxis, ax.yaxis]:
-    axis.set_major_formatter(ScalarFormatter())
-
+#Fit power law, equation calcuated in Cytoscape
 b = np.linspace(1,69,69)
 y=781.94*b**(-1.876)
-plt.loglog(b,y,'r--')
+plt.loglog(b,y,'r--') #red dotted line
+
 plt.show()
